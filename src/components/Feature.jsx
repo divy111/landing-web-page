a5094d504e43f54c13661b036d14b708673139fc
divy111 <shrivastavadivy.111@gmail.com>
2023-05-07 15:28:30 +0530

    commit again

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import FeatureBox from './FeatureBox'
+import featureimage from '../images/feature_1.jpg';
+import featureimage1 from '../images/feature_2.png';
+import featureimage2 from '../images/feature_3.png';
+
 
 function Feature() {
   return (
     <div id='features'>
         <div className='a-container'>
+          <FeatureBox image={featureimage} title = 'Novel' />
+          <FeatureBox image={featureimage1} title = 'Novel' />
+          <FeatureBox image={featureimage2} title = 'Novel' />
+
+
 
         </div>
     </div>
