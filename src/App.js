8e5063619b383176be33adf3a8baa345f30a0f4a
divy111 <shrivastavadivy.111@gmail.com>
2023-05-13 18:02:36 +0530

    contact

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About';
 import aboutimage from './images/about.png';
 import Presentation from './components/Presentation';
 import presentationimage from'./images/chat.jpg';
+import Contact from './components/Contact';
 
 
 
@@ -15,6 +16,7 @@ function App() {
       <Feature/>
       <About image={aboutimage} title='Comes with all you need' button='GET THE APP'/>
       <Presentation image={presentationimage} title='Comes with all you need'/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,47 @@
+import React, {useRef} from 'react';
+import emailjs from '@emailjs/browser';
+ 
+function Contact() {
+
+
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+    .sendForm(
+        "service_ilwsasp", 
+        "template_5fv6zi5", 
+        form.current, 
+        "wZ9woEKy9WTarPbDq"
+        )
+      .then
+      ((result) => {
+          console.log(result.text);
+          console.log("message sent");
+      }, 
+      (error) => {
+          console.log(error.text);
+      }
+      
+      );
+  };
+
+  return (
+    <div>
+        <form ref={form} onSubmit={sendEmail}>
+    <label>Name</label>
+    <input type="text" name="user_name" />
+    <label>Email</label>
+    <input type="email" name="user_email" />
+    <label>Message</label>
+    <textarea name="message" />
+    <input type="submit" value="Send" />
+  </form>
+  
+  </div>
+  )
+}
+
+export default Contact;
